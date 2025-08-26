7939bb23e4b6642be6ce72b40e05679d5a7bc090
Derek Ortiz <ortizderek708@gmail.com>
2025-08-25 22:15:40 -0600

    Cambios package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  
-})
-
+  plugins: [react(), tailwindcss()],
+  base: '/ChicosqueLloran/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          antd: ['antd'],
+        },
+      },
+    },
+  },
+});
