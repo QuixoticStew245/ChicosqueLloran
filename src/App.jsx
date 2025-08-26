6d42a39dbfab6c3bd08f13f8149d3e668dedf385
Derek Ortiz <ortizderek708@gmail.com>
2025-08-26 00:39:55 -0600

    fix: usar HashRouter y configurar vite para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRouter } from './routers/AppRouter'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter/>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
